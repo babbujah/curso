9eb8829b392530dd8ad5265773a97ff156c4bd72
Bruno Lopes <bczion@hotmail.com>
2023-03-10 16:55:29 -0300

    Conclusão da classe CollectionAggregation.

--- app/control/banco/CollectionAggregation.php
+++ app/control/banco/CollectionAggregation.php
@@ -7,9 +7,24 @@ class CollectionAggregation extends TPage{
             TTransaction::open('cursoOLD');
             TTransaction::dump();
             
-            $total = Venda::sumBy('total');
+            //$total = Venda::sumBy('total');
+            //$count = Venda::countDistinctBy('total');
+            //$rows = Venda::groupBy('dt_venda, cliente_id')->sumBy('total');
+            //$total = Venda::where('dt_venda', '>', '2015-03-12')->sumBy('total');
+            //$total = Venda::where('dt_venda', '>', '2015-03-12')->countDistinctBy('id');
+            $rows = Venda::where('dt_venda', '>', '2015-03-12')->groupBy('dt_venda')->maxBy('total');
+            $total = Venda::where('dt_venda', '>', '2015-04-12')->where('dt_venda', '<', '2019-04-12')->sumBy('total');
+            $rows = Venda::where('dt_venda', '>', '2015-04-12')->where('dt_venda', '<', '2019-04-12')->groupBy('cliente_id')->sumBy('total');
             
-            var_dump($total);            
+            
+            foreach ($rows as $row){
+                print $row->cliente_id;
+                print $row->total;
+            }
+            
+            echo '<pre>';
+            var_dump($total);
+            echo '</pre>';            
             
             TTransaction::close();
         }catch(Exception $e){
